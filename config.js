30fee38cb17eb5179b65e4df72d37bfa1d648b2a
mcalus3 <marek-calus@wp.pl>
2020-02-01 17:52:05 +0100

    Change some content

--- config.js
+++ config.js
@@ -1,51 +1,38 @@
 "use strict";
 
 module.exports = {
-  url: "https://lumen.netlify.com",
+  url: "https://mcalus.dev",
   pathPrefix: "/",
-  title: "Blog by John Doe",
-  subtitle:
-    "Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.",
+  title: "Marek Całus - Homepage",
+  subtitle: "Web developer.",
   copyright: "© All rights reserved.",
   disqusShortname: "",
   postsPerPage: 4,
-  googleAnalyticsId: "UA-73379983-2",
+  googleAnalyticsId: "",
   useKatex: false,
   menu: [
     {
-      label: "Articles",
+      label: "Blog",
       path: "/"
+    },
+    {
+      label: "My projects",
+      path: "/pages/projects"
     },
     {
       label: "About me",
       path: "/pages/about"
-    },
-    {
-      label: "Contact me",
-      path: "/pages/contacts"
     }
   ],
   author: {
-    name: "John Doe",
+    name: "Marek Całus",
     photo: "/photo.jpg",
-    bio:
-      "Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.",
+    bio: "Web developer.",
     contacts: {
       email: "",
-      facebook: "#",
-      telegram: "#",
       twitter: "#",
       github: "#",
-      rss: "",
-      vkontakte: "",
-      linkedin: "#",
-      instagram: "#",
-      line: "",
-      gitlab: "",
-      weibo: "",
-      codepen: "",
-      youtube: "",
-      soundcloud: ""
+      linkedin: "#"
     }
   }
 };
--- gatsby-config.js
+++ gatsby-config.js
@@ -70,7 +70,7 @@ module.exports = {
               })),
             query: `
               {
-                allMarkdownRemark(
+                allMdx(
                   limit: 1000,
                   sort: { order: DESC, fields: [frontmatter___date] },
                   filter: { frontmatter: { template: { eq: "post" }, draft: { ne: true } } }
